2d4f9810c0d6877c62b695b061ef7468fadfc3e0
Nadeem <nsnadeemsheriff@gamil.com>
2023-01-09 22:01:58 +0530

    new code added

---
+++ src/org/add/GreenTech.java
@@ -0,0 +1,28 @@
+package org.add;
+
+public class GreenTech {
+	private void greensOmr() {
+		System.out.println(" Greenstech omr-Balamurugan Garden, Okkiam,Landmark: ICICI Bank Upstairs, Plot No.19, Rajiv Gandhi Salai, Opposite to Sankeetha hotel, Thoraipakkam, Tamil Nadu 600097");
+	}
+	private void greensAdayar() {
+		System.out.println(" Greenstech adyar-10/15, 1st St, Padmanabha Nagar, Adyar, Chennai, Tamil Nadu 600020");
+	}
+	private void greensTambaram() {
+		System.out.println(" Greenstech tamabaram-Tambaram Sanatorium, 1, Chennai - Theni Hwy, near HP Petrol Bulk, Opposite Arulmigu Sri Rama Anjaneyar Temple, Apparao Colony, Tambaram Sanatoruim, Chennai, Tamil Nadu 600047");
+	}
+	private void greensVelacherry() {
+		System.out.println(" Greenstech velachery- #28, Nagendra Nagar, opposite Phoenix Market City, Velachery, Chennai, Tamil Nadu 600042");
+	}
+	private void greensAnnaNagar() {
+		System.out.println(" Greenstech Annanagar - 4th floor, W122, 3rd Ave, W Block, Annanagar East, Chennai, Tamil Nadu 600040");
+	}
+	public static void main(String[] args) {
+		GreenTech b = new GreenTech();
+		b.greensOmr();
+		b.greensAdayar();
+		b.greensTambaram();
+		b.greensVelacherry();
+		b.greensAnnaNagar();
+	}
+}
+
